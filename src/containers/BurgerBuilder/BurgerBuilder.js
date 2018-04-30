dd6aeecaffbf09f96a4aa83147232149b670752d
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2018-04-30 16:53:50 -0600

    add navigation button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,7 @@ class BurgerBuilder extends Component {
             <div>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                 <OrderSummary ingredients={this.state.ingredients}
+                              price={this.state.totalPrice}
                               purchaseCanceled={this.purchaseCancelHandler}
                               purchaseContinued={this.purchaseContinueHandler}/></Modal>
                 <Burger ingredients={this.state.ingredients}/>
